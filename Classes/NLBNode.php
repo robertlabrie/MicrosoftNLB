3ee850f865b4b4adb7871763f9d2eec6830823a4
unknown <Administrator@LB.contoso.local>
2013-07-30 14:20:20 -0700

    initial

---
+++ Classes/NLBNode.php
@@ -0,0 +1,69 @@
+<?php
+class NLBNode
+{
+	private $properties = Array(
+		'Caption',
+		'ComputerName',
+		'CreationClassName',
+		'DedicatedIPAddress',
+		'Description',
+		'HostPriority',
+		'InitialLoadInfo',
+		'InstallDate',
+		'LastLoadInfo',
+		'Name',
+		'NameFormat',
+		'PowerManagementCapabilities',
+		'PowerManagementSupported',
+		'PowerState',
+		'PrimaryOwnerContact',
+		'PrimaryOwnerName',
+		'ResetCapability',
+		'Roles',
+		'Status',
+		'StatusCode',
+		);
+	private $methods = Array(
+		'Disable',
+		'DisableEx',
+		'Drain',
+		'DrainEx',
+		'DrainStop',
+		'Enable',
+		'EnableEx',
+		'Resume',
+		'SetPowerState',
+		'Start',
+		'Stop',
+		'Suspend',
+	);
+	private $hostname;
+	private $com;
+	private $node;
+	public function getNode()
+	{
+		return $this->node;
+	}
+	public function __construct($hostname)
+	{
+		$this->hostname = $hostname;
+		$this->com = new COM( 'winmgmts://' . $hostname . '/root/MicrosoftNLB' );
+		$nodes = $this->com->ExecQuery("SELECT * FROM MicrosoftNLB_Node where ComputerName = '$hostname'");
+		foreach ($nodes as $node)
+		{
+			$this->node = $node;
+		}
+	}
+	public function __get($key)
+	{
+		if (in_array($key,$this->properties)) { return $this->node->$key; }
+	}
+	public function __set($key,$value)
+	{
+		if (in_array($key,$this->properties)) { $this->node->$key = $value; }
+	}
+	public function __call($name,$arguments)
+	{
+		if (in_array($key,$this->methods)) { return $this->node->$key(); }
+	}
+}
---
+++ nodescrape.php
@@ -0,0 +1,21 @@
+<?php
+include_once("./Classes/NLBNode.php");
+error_reporting(E_ALL);
+$host = "10.10.10.11";
+
+$obj = new COM( 'winmgmts://' . $host . '/root/MicrosoftNLB' );
+
+$nodes = $obj->ExecQuery("SELECT * FROM MicrosoftNLB_Node");
+$i = 0;
+foreach ($nodes as $node)
+{
+	
+	//echo $node->ComputerName . "\t" . $node->DedicatedIPAddress . "\t" . $node->StatusCode . "\n";
+	$oNode[$i] = new NLBNode($node->ComputerName);
+	$n = $oNode[$i]->getNode();
+	$n->Stop();
+	echo $oNode[$i]->ComputerName . "\t" . $oNode[$i]->DedicatedIPAddress . "\t" . $oNode[$i]->StatusCode . "\n";
+	//$oNode[$i]->Stop();
+	$i++;
+}
+
